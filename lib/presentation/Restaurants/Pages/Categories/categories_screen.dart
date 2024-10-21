b881fdb025f80a3c03d4faf5f698225dc747172d
YCS <testdemo7260@gmail.com>
2024-10-21 17:56:04 +0530

    update

--- lib/presentation/Restaurants/Pages/Categories/categories_screen.dart
+++ lib/presentation/Restaurants/Pages/Categories/categories_screen.dart
@@ -14,6 +14,76 @@ class CategoriesScreen extends StatelessWidget {
           style: AppFontStyle.text_28_600(AppColors.darkText),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(width: 1,color: AppColors.textFieldBorder)
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20,),
+
+            // Category list
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: AppColors.lightPrimary
+                      ),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 100,
+                            child: Row(
+                              children: [
+                                Container(
+                                    child: Image.asset("assets/images/6cee589c2f553320ee93e5afced09766 1.png",scale: 10,fit: BoxFit.fill,),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  height: 80,
+                                  width: 80,
+                                ),
+                                SizedBox(width: 10,),
+                                Text('Pizza')
+                              ],
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward_ios),
+                        ],
+                      ),
+                    ),
+                  );
+                }, separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(height: 20,);
+              },
+              ),
+            ),
+            SizedBox(height: 70,)
+          ],
+        ),
+      ),
     );
   }
 }
