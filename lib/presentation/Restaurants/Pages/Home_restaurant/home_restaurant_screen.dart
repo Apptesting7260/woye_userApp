9ba4ba995e7723147307404b810283360f1c57e9
Apptesting7260 <testdemo7260@gmail.com>
2024-10-23 11:06:31 +0530

    merging-again

--- lib/presentation/Restaurants/Pages/Home_restaurant/home_restaurant_screen.dart
+++ lib/presentation/Restaurants/Pages/Home_restaurant/home_restaurant_screen.dart
@@ -1,6 +1,4 @@
-import 'package:sticky_headers/sticky_headers/widget.dart';
 import 'package:woye_user/Core/Utils/app_export.dart';
-import 'package:woye_user/Presentation/Common/Home/home_screen.dart';
 
 class HomeRestaurantScreen extends StatefulWidget {
   const HomeRestaurantScreen({super.key});
@@ -21,7 +19,7 @@ class _HomeRestaurantScreenState extends State<HomeRestaurantScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(_getHeight);
+    // WidgetsBinding.instance.addPostFrameCallback(_getHeight);
   }
 
   @override
@@ -29,34 +27,34 @@ class _HomeRestaurantScreenState extends State<HomeRestaurantScreen> {
     return Scaffold(
       body: Column(
         children: [
-          HomeScreen(
-            key: homeWidgetKey,
-          ),
+          // HomeScreen(
+          //   key: homeWidgetKey,
+          // ),
           SizedBox(
-            height: Get.height - height,
+            // height: Get.height - height,
             child: ListView(
               children: [
-                StickyHeader(
-                  header: Container(
-                    height: 50.0,
-                    color: Colors.blueGrey[700],
-                    padding: EdgeInsets.symmetric(horizontal: 16.0.h),
-                    alignment: Alignment.centerLeft,
-                    child: const Text(
-                      'Header #',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  content: SizedBox(
-                    height: 40,
-                    child: Image.asset(
-                      ImageConstants.restaurantDisable,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: 200.0,
-                    ),
-                  ),
-                ),
+                // StickyHeader(
+                //   header: Container(
+                //     height: 50.0,
+                //     color: Colors.blueGrey[700],
+                //     padding: EdgeInsets.symmetric(horizontal: 16.0.h),
+                //     alignment: Alignment.centerLeft,
+                //     child: const Text(
+                //       'Header #',
+                //       style: TextStyle(color: Colors.white),
+                //     ),
+                //   ),
+                //   content: SizedBox(
+                //     height: 40,
+                //     child: Image.asset(
+                //       ImageConstants.restaurantDisable,
+                //       fit: BoxFit.cover,
+                //       width: double.infinity,
+                //       height: 200.0,
+                //     ),
+                //   ),
+                // ),
                 const Text("data"),
                 const Text("data"),
                 const Text("data"),
