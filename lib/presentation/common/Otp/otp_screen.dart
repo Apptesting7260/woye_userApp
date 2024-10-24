a332574de5c2eb1987e81a53e6a3573414df7866
Shivam <testdemo7260@gmail.com>
2024-10-24 16:20:56 +0530

    again new otp

--- lib/presentation/common/Otp/otp_screen.dart
+++ lib/presentation/common/Otp/otp_screen.dart
@@ -77,35 +77,6 @@ class OtpScreen extends StatelessWidget {
               ),
             ),
             hBox(20),
-            // Align(
-            //     alignment: Alignment.center,
-            //     child: otpController.duration != 0
-            //         ?
-            //         //     ? Text(
-            //         //         "${(otpVerificationController.duration ~/ 60).toString().padLeft(2, '0')}:${(otpVerificationController.duration % 60).toString().padLeft(2, '0')}",
-            //         //         // "00:${otpVerificationController.start.toString().padLeft(2, "0")}",
-            //         //         style: TextStyle(
-            //         //             color: const Color.fromARGB(255, 184, 30, 30),
-            //         //             fontSize: 15.sp,
-            //         //             fontWeight: FontWeight.w500),
-            //         //       )
-            //         //     : null
-            //         Text(
-            //             "Resend code in ${otpController.duration.toString()} s",
-            //             style: AppFontStyle.text_16_400(AppColors.darkText),
-            //           )
-            //         : GestureDetector(
-            //             onTap: () {
-            //               // from == 'login'
-            //               //     ? loginController.rese
-            //               //     : signUpController.verificationID.value
-            //
-            //             },
-            //             child: Text(
-            //               "Resend Code",
-            //               style: AppFontStyle.text_16_400(AppColors.lightText),
-            //             ),
-            //           ))
             from == 'login'
                 ? Align(
                     alignment: Alignment.center,
