db13d638bb2541bc7df011f5720af726d9d8e0a8
Apptesting7260 <testdemo7260@gmail.com>
2024-10-24 14:26:13 +0530

    good-afternoon

--- lib/presentation/Restaurants/Pages/Restaurant_home/restaurant_home_controller.dart
+++ lib/presentation/Restaurants/Pages/Restaurant_home/restaurant_home_controller.dart
@@ -1,3 +1,24 @@
 import 'package:woye_user/Core/Utils/app_export.dart';
 
-class RestaurantHomeController extends GetxController {}
+class RestaurantHomeController extends GetxController {
+  List<Map<dynamic, dynamic>> restaurantList = [
+    {
+      "title": "The Pizza Hub And Restaurants",
+      "type": "Pure veg",
+      "image": "assets/images/restaurant-1.png",
+      "isFavourite": false
+    },
+    {
+      "title": "Casa Della Saucy",
+      "type": "Veg and Non Veg",
+      "image": "",
+      "isFavourite": false
+    },
+    {
+      "title": "The Royal Restaurants",
+      "type": "Pure veg",
+      "image": "",
+      "isFavourite": false
+    },
+  ];
+}
