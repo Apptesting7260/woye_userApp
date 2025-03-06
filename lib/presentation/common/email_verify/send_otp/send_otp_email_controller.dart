b3b7c7d8b651e7b36536d6642d0da9fee8ed7fa5
Shivam <testdemo7260@gmail.com>
2025-03-06 18:59:47 +0530

    6 march 2025

--- lib/presentation/common/email_verify/send_otp/send_otp_email_controller.dart
+++ lib/presentation/common/email_verify/send_otp/send_otp_email_controller.dart
@@ -1,3 +1,4 @@
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:woye_user/Core/Utils/app_export.dart';
 import 'package:woye_user/presentation/common/Update_profile/controller/Update_profile_controller.dart';
 import 'package:woye_user/presentation/common/email_verify/send_otp/send_otp_modal.dart';
@@ -5,10 +6,13 @@ import 'package:woye_user/presentation/common/email_verify/send_otp/send_otp_mod
 class SendOtpEmailController extends GetxController {
   final api = Repository();
   final rxRequestStatus = Status.COMPLETED.obs;
+  final rxRequestStatus1 = Status.COMPLETED.obs;
   final sendOtpData = SendOtpModal().obs;
   RxString error = ''.obs;
 
   void setRxRequestStatus(Status value) => rxRequestStatus.value = value;
+
+  void setRxRequestStatus1(Status value) => rxRequestStatus1.value = value;
 
   void setData(SendOtpModal value) => sendOtpData.value = value;
 
@@ -34,6 +38,29 @@ class SendOtpEmailController extends GetxController {
     });
   }
 
+  Future resendSendOtpApi({required String email}) async {
+    setRxRequestStatus1(Status.LOADING);
+    var body = {
+      "email": email,
+    };
+    await api.sendVerificationOtp(body).then((value) {
+      setData(value);
+      if (sendOtpData.value.status == true) {
+        setRxRequestStatus1(Status.COMPLETED);
+        startTimer();
+        showOtpVerificationRequired(email);
+      } else {
+        setRxRequestStatus1(Status.COMPLETED);
+        Utils.showToast(sendOtpData.value.message.toString());
+      }
+    }).onError((error, stackError) {
+      print("Error: $error");
+      setError(error.toString());
+      print(stackError);
+      setRxRequestStatus1(Status.ERROR);
+    });
+  }
+
   void setError(String value) => error.value = value;
   final Rx<TextEditingController> otpVerifyController =
       TextEditingController().obs;
@@ -55,132 +82,145 @@ class SendOtpEmailController extends GetxController {
     });
   }
 
-
   Future showOtpVerificationRequired(email) {
     return showDialog(
-      barrierDismissible: false,
-      context: Get.context!,
-      builder: (context){
-        return PopScope(
-          canPop: false,
-          child: AlertDialog(
-            contentPadding: EdgeInsets.zero,
-            insetPadding: EdgeInsets.zero,
-            content: Container(
-              height: 310.h,
-              width: 320.w,
-              padding: REdgeInsets.symmetric(vertical: 15, horizontal: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.r),
-              ),
-              child: Padding(
-                padding: REdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'OTP Verification',
-                      style: AppFontStyle.text_20_600(AppColors.darkText),
-                    ),
-                    hBox(10.h),
-                    RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(text: "Please enter the verification code sent to", style: AppFontStyle.text_14_400(AppColors.lightText), ),
-                            TextSpan(text: "\n$email", style: AppFontStyle.text_14_400(AppColors.primary)),
-                          ]
-                        )),
-                    Row(mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        InkWell(
-                            onTap: (){
-                              _timer.cancel();
-                              isResendEnabled.value = false;
-                              remainingTime.value = 60;
-                              print( remainingTime.value);
-                              Get.back();
-                            },
-                            child: Text("Wrong email?", style: AppFontStyle.text_14_400(AppColors.primary)))
-                      ],
-                    ),
-                    hBox(10.h),
-                    Pinput(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      length: 4,
-                      controller: otpVerifyController.value,
-                      defaultPinTheme: defaultPinTheme,
-                      focusedPinTheme: focusedPinTheme,
-                      submittedPinTheme: submittedPinTheme,
-                      // defaultPinTheme: otpController.defaultPinTheme,
-                      // focusedPinTheme: otpController.focusedPinTheme,
-                      // submittedPinTheme: otpController.submittedPinTheme,
-                    ),
-                    hBox(10.h),
-
-                    // Container(
-                    //   width: double.infinity,
-                    //   padding: REdgeInsets.symmetric(horizontal: 20),
-                    //   child: TextField(
-                    //     controller: otpVerifyController.value,
-                    //     decoration: InputDecoration(
-                    //       hintText: 'Enter OTP',
-                    //       border: OutlineInputBorder(
-                    //         borderRadius: BorderRadius.circular(10.r),
-                    //       ),
-                    //       filled: true,
-                    //       fillColor: AppColors.lightText.withOpacity(0.1),
-                    //     ),
-                    //     keyboardType: TextInputType.number,
-                    //     textAlign: TextAlign.center,
-                    //   ),
-                    // ),
-                    // hBox(15),
-                    Obx(() =>
-                      CustomElevatedButton(
-                        isLoading: (rxRequestStatus2.value == Status.LOADING),
-                        height: 50.h,
-                        width: Get.width / 3,
-                        onPressed: () {
-                          verifyOtpApi(
-                            otp: otpVerifyController.value.text.trim(),
-                            email: email,
-                          );
-                        },
-                        text: "Verify",
+        barrierDismissible: false,
+        context: Get.context!,
+        builder: (context) {
+          return PopScope(
+            canPop: false,
+            child: AlertDialog(
+              contentPadding: EdgeInsets.zero,
+              insetPadding: EdgeInsets.zero,
+              content: Container(
+                height: 310.h,
+                width: 320.w,
+                padding: REdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.r),
+                ),
+                child: Padding(
+                  padding: REdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'OTP Verification',
+                        style: AppFontStyle.text_20_600(AppColors.darkText),
                       ),
-                    ),
-                    Obx(() =>
-                      TextButton(
-                        onPressed: isResendEnabled.value
-                        ? () {
-                          debugPrint('Resending OTP');
-                          startTimer();
-                          sendOtpApi(email:email);
-                        }
-                            : null,
-                        child: Text(
-                            isResendEnabled.value
-                                ? 'Resend code'
-                                : 'Resend code in ${remainingTime.value} sec',
-                            style: TextStyle(color: AppColors.darkText,
-                              decoration: isResendEnabled.value ? TextDecoration.underline : TextDecoration.none,
-                              fontSize: 16.sp,
-                            )
-                          // style: AppFontStyle.text_16_400(
-                          //    AppColors.darkText,
-                          //   fontFamily: AppFontFamily.gilroyRegular,
-                          //   decoration: TextDecoration.underline,
-                          // ),
+                      hBox(10.h),
+                      RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                          text: "Please enter the verification code sent to",
+                          style: AppFontStyle.text_14_400(AppColors.lightText),
+                        ),
+                        TextSpan(
+                            text: "\n$email",
+                            style: AppFontStyle.text_14_400(AppColors.primary)),
+                      ])),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          InkWell(
+                              onTap: () {
+                                _timer.cancel();
+                                isResendEnabled.value = false;
+                                remainingTime.value = 60;
+                                print(remainingTime.value);
+                                Get.back();
+                              },
+                              child: Text("Wrong email?",
+                                  style: AppFontStyle.text_14_400(
+                                      AppColors.primary)))
+                        ],
+                      ),
+                      hBox(10.h),
+                      Pinput(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        length: 4,
+                        controller: otpVerifyController.value,
+                        defaultPinTheme: defaultPinTheme,
+                        focusedPinTheme: focusedPinTheme,
+                        submittedPinTheme: submittedPinTheme,
+                        // defaultPinTheme: otpController.defaultPinTheme,
+                        // focusedPinTheme: otpController.focusedPinTheme,
+                        // submittedPinTheme: otpController.submittedPinTheme,
+                      ),
+                      hBox(10.h),
+
+                      // Container(
+                      //   width: double.infinity,
+                      //   padding: REdgeInsets.symmetric(horizontal: 20),
+                      //   child: TextField(
+                      //     controller: otpVerifyController.value,
+                      //     decoration: InputDecoration(
+                      //       hintText: 'Enter OTP',
+                      //       border: OutlineInputBorder(
+                      //         borderRadius: BorderRadius.circular(10.r),
+                      //       ),
+                      //       filled: true,
+                      //       fillColor: AppColors.lightText.withOpacity(0.1),
+                      //     ),
+                      //     keyboardType: TextInputType.number,
+                      //     textAlign: TextAlign.center,
+                      //   ),
+                      // ),
+                      // hBox(15),
+                      Obx(
+                        () => CustomElevatedButton(
+                          isLoading: (rxRequestStatus2.value == Status.LOADING),
+                          height: 50.h,
+                          width: Get.width / 3,
+                          onPressed: () {
+                            verifyOtpApi(
+                              otp: otpVerifyController.value.text.trim(),
+                              email: email,
+                            );
+                          },
+                          text: "Verify",
                         ),
                       ),
-                    ),
-                  ],
+                      Obx(
+                        () => TextButton(
+                          onPressed: isResendEnabled.value
+                              ? () {
+                                  debugPrint('Resending OTP');
+
+                                  resendSendOtpApi(email: email);
+                                }
+                              : null,
+                          child: (rxRequestStatus1.value == Status.LOADING)
+                              ? LoadingAnimationWidget.inkDrop(
+                                  color: AppColors.primary,
+                                  size: 10.h,
+                                )
+                              : Text(
+                                  isResendEnabled.value
+                                      ? 'Resend code'
+                                      : 'Resend code in ${remainingTime.value} sec',
+                                  style: TextStyle(
+                                    color: AppColors.darkText,
+                                    decoration: isResendEnabled.value
+                                        ? TextDecoration.underline
+                                        : TextDecoration.none,
+                                    fontSize: 16.sp,
+                                  )
+                                  // style: AppFontStyle.text_16_400(
+                                  //    AppColors.darkText,
+                                  //   fontFamily: AppFontFamily.gilroyRegular,
+                                  //   decoration: TextDecoration.underline,
+                                  // ),
+                                  ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        );
-    });
+          );
+        });
   }
 
   final rxRequestStatus2 = Status.COMPLETED.obs;
